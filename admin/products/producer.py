a35628a7c41662b073aa69a747ab738c74421152
jade <bleachjade@hotmail.com>
2021-04-23 13:27:04 +0700

    finish the microservices assignment

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+
+import pika, json
+
+params = pika.URLParameters('amqps://kpsgfwzg:aqq0NUELhecCOJ9dxg0JGRMmLmXvPLnD@clam.rmq.cloudamqp.com/kpsgfwzg')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
